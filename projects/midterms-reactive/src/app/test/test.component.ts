625e59aad7e7e82945301d3d1dbcdf6b001d3ea6
Katrine Joy Dela Cruz <kdelacruz.ivyleague@gmail.com>
2022-03-17 05:56:22 +0800

    Updated Angular Pipes

---
+++ projects/midterms-reactive/src/app/test/test.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+// Import rxjs for Async Pipe
+import { Observable, Observer } from 'rxjs';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  presentDate = new Date();
+
+  // Set Parameters for Async Date Pipe
+  time = new Observable<string>((observer: Observer<string>) => {
+    setInterval(() => observer.next(new Date().toString()), 1000);
+  });
+
+  // Currency Pipe
+  price: number = 20000;
+
+  // Slice Pipe
+  Fruits = ['Apple', 'Orange', 'Grapes', 'Mango', 'Kiwi', 'Pomegranate'];
+  
+  // Decimal Pipe
+  decimalNum1: number = 9.7589623;
+  decimalNum2: number = 5.43;
+
+  // Percent Pipe
+  num1: number = 0.259;
+  num2: number = 1.3495;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
